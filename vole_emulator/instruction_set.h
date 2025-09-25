c9bb72ede96af1d887bb6bd98183e51a32ab9c87
Alex A. <frailxup@gmail.com>
2025-09-25 12:21:21 -0400

    added struct data type to fix missing identifier issue.

--- vole_emulator/instruction_set.h
+++ vole_emulator/instruction_set.h
@@ -1,16 +1,14 @@
 #ifndef instruction_set_h
 #define instruction_set_h
 #include <stdint.h>
-#include <stdio.h>
+#include <iostream>
 #include "cell.h"
 #include "machine.h"
-
-
 
 // 16-bit instuction set for Vole
 struct instruction_set {
 	// 0X1: Load register r with the bit pattern found in the memory cell of address
-	void load(machine m, uint8_t register_number, uint8_t address);
+	void load(struct machine m, uint8_t register_number, uint8_t address);
 };
 
 #endif
