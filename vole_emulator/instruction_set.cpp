043443b76a08545fdaaa1d05bd7da2d0944d740e
Alex A. <frailxup@gmail.com>
2025-09-26 15:58:59 -0400

    Instruction 0x2 complete.

--- vole_emulator/instruction_set.cpp
+++ vole_emulator/instruction_set.cpp
@@ -1,13 +1,25 @@
 #include "instruction_set.h"
 
-void instruction_set::load(struct machine m, uint8_t register_number, uint8_t address) {
+void instruction_set::load_0x1(struct machine m, uint8_t register_number, uint8_t address) {
 	bool use_register = m.CPU.registers.valid_register(register_number);
 	bool use_address = m.cell.valid_address(address);
 
 	if (use_register && use_address) {
-		std::cout << "0x1 - LOAD" << std::endl;
+		std::cout << REGISTER_0 << " - LOAD" << std::endl;
 		m.CPU.registers.registers[register_number] = m.cell.cells[address];
 		std::cout << "Contents of memory cell " << std::hex << (int)address;
 		std::cout << " stored in register " << std::hex << (int)register_number;
+	}
+}
+
+void instruction_set::load_0x2(struct machine m, uint8_t address) {
+	bool use_register = m.CPU.registers.valid_register(0x1);
+	bool use_address = m.cell.valid_address(address);
+
+	if (use_register && use_address) {
+		std::cout << "0x2 - LOAD" << std::endl;
+		m.CPU.registers.registers[REGISTER_0] = m.cell.cells[address];
+		std::cout << "Contents of memory cell " << std::hex << (int)address;
+		std::cout << " stored in register " << std::hex << (int)0x1;
 	}
 }
--- vole_emulator/instruction_set.h
+++ vole_emulator/instruction_set.h
@@ -1,5 +1,6 @@
 #ifndef instruction_set_h
 #define instruction_set_h
+#define REGISTER_0 0x1
 #include <stdint.h>
 #include <iostream>
 #include "cell.h"
@@ -8,7 +9,10 @@
 // 16-bit instuction set for Vole
 struct instruction_set {
 	// 0X1: Load register r with the bit pattern found in the memory cell of address
-	void load(struct machine m, uint8_t register_number, uint8_t address);
+	void load_0x1(struct machine m, uint8_t register_number, uint8_t address);
+
+	// 0x2: Load register 0 with the bit pattern found in the memory cell of address
+	void load_0x2(struct machine m, uint8_t address);
 };
 
 #endif
